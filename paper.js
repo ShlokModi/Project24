ead54d60d530a6c37f1322803c4f01b56c3b0975
ShlokModi <65662601+ShlokModi@users.noreply.github.com>
2020-06-08 17:59:31 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper{
+    constructor(){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:0.2,
+        }
+        this.body = Bodies.circle(50,330,20,options);
+        this.diameter = 20;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        //push();
+        //translate(pos.x,pos.y);
+        //rotate(angle);
+        //ellipseMode(RADIUS);
+        ellipse(pos.x,pos.y,40,40);
+        pop();
+    }
+};
